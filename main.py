8053f88fc7649f1a42704b1e9c67e13d68b7a816
yuriy-mokshin <yuriy.mokshin@gmail.com>
2023-11-04 01:52:46 +0200

    Added new files.

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import cv2
+import pyautogui
+import time
 
 
-def qr_read(image):
-    picture = cv2.imread(image)
+def get_screenshot(image_name):
+    image = pyautogui.screenshot(region=(1375, 840, 200, 200))
+    image.save(image_name)
+
+
+def qr_read(image_name):
+    picture = cv2.imread(image_name)
     detect = cv2.QRCodeDetector()
     link, _, _, = detect.detectAndDecode(picture)
     return link
 
 
 def main():
-    print(f' [+] - link = {qr_read("qr.png")}')
+    #
+    time.sleep(5)
+    print(' [+] - start - ')
+    for i in range(1):
+        print(f' [+] - step #{str(i)}')
+        get_screenshot(f"qr{str(i)}.png")
+        time.sleep(1)
+        pyautogui.click()
+        time.sleep(1)
+        print(f' [+] - link = {qr_read(f"qr{str(i)}.png")}')
+    print(' [+] - finish - ')
 
 
 if __name__ == "__main__":
